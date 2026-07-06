35370865b206f9d1abdc7199195a2e1555e50e4a
a01287210-eng <a01287210@tec.mx>
2026-07-05 23:58:46 -0600

    Personaje .hpp

--- assignments/Proyecto/Personaje.hpp
+++ assignments/Proyecto/Personaje.hpp
@@ -1,1 +1,33 @@
-// Crear el archivo header de la clase Personaje, no olvides las guardas o el pragma.
+#ifndef UNIDAD_HPP
+#define UNIDAD_HPP
+ 
+class Personaje {
+private:
+    int vida;
+    int salud;
+    int ataque;
+    int nivel;
+ 
+public:
+    Personaje();
+    Personaje(int v, int a, int n);
+ 
+    int getVida();
+    int getSalud();
+    int getAtaque();
+    int getNivel();
+ 
+    void setVida(int v);
+    void setSalud(int s);
+    void setAtaque(int a);
+    void setNivel(int n);
+ 
+    int porcentajeSalud();
+    void imprimeBarra();
+    int calculaAtaque(Personaje& objetivo);
+    void recibeAtaque(int puntos);
+    void atacar(Personaje& objetivo);
+    void imprimir();
+};
+ 
+#endif// Crear el archivo header de la clase Personaje, no olvides las guardas o el pragma.
